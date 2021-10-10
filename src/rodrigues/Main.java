3cdda473251b5474e009397fe1c93b4e1a74e4c4
michelleguti17 <gut20014@byui.edu>
2021-10-09 22:17:11 -0500

    Hello from Michelle

--- src/rodrigues/Main.java
+++ src/rodrigues/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my name is Helaman");
+        System.out.println("Hello Helaman, my name is Michelle");
     }
 }
