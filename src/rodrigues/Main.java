6fe47e9817977ea6f12a42562208df468b410147
Christopher Mendes <bos21001@byui.edu>
2021-10-10 00:52:08 -0300

    Christopher Mendes

--- src/rodrigues/Main.java
+++ src/rodrigues/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is Helaman");
         System.out.println("Hello Helaman, my name is Michelle");
+        System.out.println("Hello Helaman, my name is Christopher");
     }
 }
