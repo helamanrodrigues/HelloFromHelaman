afebf83e3d71ddd748717a3042e76089aca91a7b
Marco Novaes <mako1tube2@gmail.com>
2021-10-10 01:34:22 -0300

    Marco Novaes name change

--- src/rodrigues/Main.java
+++ src/rodrigues/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Hello my name is Helaman");
         System.out.println("Hello Helaman, my name is Michelle");
         System.out.println("Hello Helaman, my name is Christopher");
-        System.out.println("Hello my name is Marco");
+        System.out.println("Hello my name is Marco Novaes");
     }
 }
